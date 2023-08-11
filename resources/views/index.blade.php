e69722bc36e15eb9e96b77c771c3d1cbba368b70
ludon89 <daudonlucas@gmail.com>
2023-08-11 11:55:36 +0200

    création page d'accueil/test

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+
+  <div class="py-12">
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <p>Coucou !</p>
+    </div>
+  </div>
+
+</x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
       <div class="flex">
         <!-- Logo -->
         <div class="flex shrink-0 items-center">
-          <a href="{{ route('dashboard') }}">
+          <a href="{{ route('index') }}">
             <x-application-logo
               class="block h-9 w-auto fill-current text-gray-800" />
           </a>
