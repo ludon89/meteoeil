1e02178abc1d48eed24283b679638cb30cfd7532
ludon89 <daudonlucas@gmail.com>
2023-08-15 15:36:16 +0200

    prep route ressource

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
   <div class="py-12">
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <p>Coucou !</p>
+      <p>Hello World!</p>
     </div>
   </div>
 
--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,25 @@ Route::get('/', function () {
     return view('index');
 })->name('index');
 
-Route::get('/welcome', function () {
-    return view('welcome');
-})->name('welcome');
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+// ==================== //
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+// ==================== //
+
+Route::get('/welcome', function () {
+    return view('welcome');
+})->name('welcome');
+
 
 require __DIR__ . '/auth.php';
