4b9f53d8dfaf5ee723beb625ff7908a916edc38f
ludon89 <daudonlucas@gmail.com>
2023-08-16 11:53:38 +0200

    config UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,11 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'age' => fake()->numberBetween(18, 80),
+            'gender' => fake()->randomElement(['homme', 'femme', 'autre']),
+            'location' => fake()->city(),
+            'avatar' => '',
+            'is_admin' => false,
         ];
     }
 
