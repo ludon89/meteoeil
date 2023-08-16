6ef9d018c4bc58a77dab20e92bb315603802302c
ludon89 <daudonlucas@gmail.com>
2023-08-16 09:39:22 +0200

    ajout modèle Comment et remplissage  Observation

--- app/Models/Observation.php
+++ app/Models/Observation.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Observation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'location',
+        'datetime',
+        'departement',
+        'weather',
+        'temperature',
+        'picture',
+        'title',
+        'description',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
