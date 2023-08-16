4ddb0d37fcf10923f23c8b3217f6fb7b391795f0
ludon89 <daudonlucas@gmail.com>
2023-08-16 09:16:20 +0200

    setup police Raleway

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+        sans: ["Raleway", ...defaultTheme.fontFamily.sans],
       },
     },
   },
