0147ebff11b52a51f04fa52b2f187e6c12f7f2ff
ludon89 <daudonlucas@gmail.com>
2023-08-31 10:54:22 +0200

    aria labels navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
 <div
   class="z-50 flex w-full flex-wrap border-b border-gray-100 bg-white py-4 text-sm dark:bg-gray-800 sm:flex-nowrap sm:justify-start">
   <nav class="mx-auto flex w-full justify-between px-2 sm:items-center"
-    aria-label="Global">
+    aria-label="Menu principal">
     <div class="flex w-full items-center justify-between">
       <!-- Logo & tagline -->
       <a class="inline-flex items-center gap-x-2 text-xl font-semibold dark:text-white"
@@ -21,15 +21,16 @@
         class="hs-collapse hidden grow basis-full overflow-hidden transition-all duration-300 sm:block">
         <div
           class="mt-5 flex flex-col gap-5 sm:mt-0 sm:flex-row sm:items-center sm:justify-end sm:px-5">
-          <a class="font-medium text-blue-500" href="{{ route('index') }}"
-            aria-current="page">Accueil</a>
+          <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+            href="{{ route('index') }}">Accueil</a>
           <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
             href="{{ route('observations.create') }}">Poster une observation</a>
         </div>
       </div>
       <!-- Dropdown -->
       <div class="hs-dropdown relative inline-flex">
-        <button id="hs-dropdown-custom-icon-trigger" type="button"
+        <button id="menu-button" aria-haspopup="true" aria-expanded="false"
+          aria-label="Menu déroulant" type="button"
           class="hs-dropdown-toggle inline-flex h-10 w-10 items-center justify-center gap-2 rounded-md border bg-white p-3 align-middle text-sm font-medium text-gray-700 shadow-sm transition-all hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white dark:border-gray-700 dark:bg-slate-900 dark:text-gray-400 dark:hover:bg-slate-800 dark:hover:text-white dark:focus:ring-offset-gray-800">
           <svg class="h-4 w-4 text-gray-600" xmlns="http://www.w3.org/2000/svg"
             width="16" height="16" fill="currentColor"
@@ -40,9 +41,9 @@
         </button>
         <div
           class="hs-dropdown-menu duration z-50 mt-2 hidden min-w-[15rem] rounded-lg bg-white p-2 opacity-0 shadow-md transition-[opacity,margin] hs-dropdown-open:opacity-100 dark:border dark:border-gray-700 dark:bg-gray-800"
-          aria-labelledby="hs-dropdown-with-header">
+          aria-labelledby="menu-button" id="dropdown-menu" role="menu">
           @auth
-            <a href="{{ route('profile.edit') }}">
+            <a href="{{ route('profile.edit') }}" role="menuitem">
               <div
                 class="-m-2 rounded-t-lg bg-gray-100 px-5 py-3 dark:bg-gray-700">
                 <p class="text-sm text-gray-500 dark:text-gray-400">Connecté en
@@ -55,28 +56,28 @@
           @endauth
           <div class="mt-2 py-2 first:pt-0 last:pb-0">
             <a class="flex items-center gap-x-3.5 rounded-md px-3 py-2 text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 sm:hidden"
-              href="{{ route('index') }}">
+              href="{{ route('index') }}" role="menuitem">
               Accueil
             </a>
             <a class="flex items-center gap-x-3.5 rounded-md px-3 py-2 text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 sm:hidden"
-              href="{{ route('observations.create') }}">
+              href="{{ route('observations.create') }}" role="menuitem">
               Poster une observation
             </a>
             @auth
               <a class="flex items-center gap-x-3.5 rounded-md px-3 py-2 text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                href="{{ route('dashboard') }}">
+                href="{{ route('dashboard') }}" role="menuitem">
                 Vos observations
               </a>
               @if (Auth::user()->is_admin)
                 <a class="flex items-center gap-x-3.5 rounded-md px-3 py-2 text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                  href="{{ route('admin.dashboard') }}">
+                  href="{{ route('admin.dashboard') }}" role="menuitem">
                   Dashboard admin
                 </a>
               @endif
               <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <a class="flex items-center gap-x-3.5 rounded-md px-3 py-2 text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                  href="{{ route('logout') }}"
+                  href="{{ route('logout') }}" role="menuitem"
                   onclick="event.preventDefault();
                              this.closest('form').submit();">
                   Déconnexion
@@ -86,11 +87,11 @@
 
             @guest
               <a class="flex items-center gap-x-3.5 rounded-md px-3 py-2 text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                href="{{ route('login') }}">
+                href="{{ route('login') }}" role="menuitem">
                 Connexion
               </a>
               <a class="flex items-center gap-x-3.5 rounded-md px-3 py-2 text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                href="{{ route('register') }}">
+                href="{{ route('register') }}" role="menuitem">
                 Inscription
               </a>
             @endguest
